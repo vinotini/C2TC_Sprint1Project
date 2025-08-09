495712f150beec5235c701db5e2a0c125dfd7671
Vinotini <146613025+vinotini@users.noreply.github.com>
2025-08-09 20:48:31 +0530

    Delete customerservice directory

--- customerservice/src/main/java/com/tnsif/customerservice/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tnsif.customerservice.repository;
-
-import com.tnsif.customerservice.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
